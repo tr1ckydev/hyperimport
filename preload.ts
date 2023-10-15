50c90ffefead3a735946e5acf9b41921c4af26d2
Revenity <aquaplmc@gmail.com>
2023-10-15 15:57:43 +0700

    Update preload.ts

--- preload.ts
+++ preload.ts
@@ -1,30 +1,34 @@
 import { HyperImportConfig } from "./src/types";
 import { debugLog } from "./src/utils";
 
-const cwd = process.cwd(),
-    config: HyperImportConfig = (await import(`${cwd}/bunfig.toml`)).default.hyperimport;
+const cwd = process.cwd();
+const config: HyperImportConfig = (await import(`${cwd}/bunfig.toml`)).default.hyperimport;
 
 debugLog(config.debug, 3, "registering loaders...");
 
-for (const loader of config.loaders ?? []) 
-    try {
-        const l = await import(`./src/loaders/${loader}.ts`),
-            plugin = await new l.default(cwd).toPlugin();
+/**
+ * Register a plugin
+ * @param loader
+ * @param path A path to register
+ * @param custom Whether it is a custom plugin
+ */
+async function register(loader, path: string, custom: boolean) {
+    const isCustom = custom ? "[CUSTOM] " : "";
     
-        Bun.plugin(plugin);
-        debugLog(config.debug, 2, plugin.name, "has been registered.");
-    } catch (e) {
-        debugLog(config.debug, 1, "loader not found:", loader);
-    }
-
-
-for (const loader of config.custom ?? []) 
     try {
-        const l = await import(`${cwd}/${loader}`),
-            plugin = await new l.default(cwd).toPlugin();
-        
+        const l = await import(path);
+        const plugin = await new l.default(cwd).toPlugin();
+
         Bun.plugin(plugin);
-        debugLog(config.debug, 2, "[CUSTOM]", plugin.name, "has been registered.");
+        debugLog(config.debug, 2, isCustom + plugin.name, "has been registered.");
     } catch (e) {
-        debugLog(config.debug, 1, "[CUSTOM] loader not found:", loader);
+        debugLog(config.debug, 1, isCustom + "loader not found:", loader);
     }
+
+// Register default loader
+for (const loader of config.loaders ?? []) 
+    register(loader, `./src/loaders/${loader}.ts`, false);
+
+// Register custom loader
+for (const loader of config.custom ?? []) 
+    register(loader, `${cwd}/${loader}`, true);
