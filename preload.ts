cb39a72eacc666bc82dbffced78ee8dd50c1f135
Revenity <aquaplmc@gmail.com>
2023-10-15 09:27:50 +0700

    Update preload.ts

--- preload.ts
+++ preload.ts
@@ -1,23 +1,30 @@
 import { HyperImportConfig } from "./src/types";
 import { debugLog } from "./src/utils";
 
-const cwd = process.cwd();
-const config: HyperImportConfig = (await import(`${cwd}/bunfig.toml`)).default.hyperimport;
+const cwd = process.cwd(),
+    config: HyperImportConfig = (await import(`${cwd}/bunfig.toml`)).default.hyperimport;
 
 debugLog(config.debug, 3, "registering loaders...");
 
-for (const loader of config.loaders ?? []) {
-    await import(`./src/loaders/${loader}.ts`).then(async l => {
-        const plugin = await new l.default(cwd).toPlugin();
+for (const loader of config.loaders ?? []) 
+    try {
+        const l = await import(`./src/loaders/${loader}.ts`),
+            plugin = await new l.default(cwd).toPlugin();
+    
         Bun.plugin(plugin);
         debugLog(config.debug, 2, plugin.name, "has been registered.");
-    }).catch(() => debugLog(config.debug, 1, "loader not found:", loader));
-}
+    } catch (e) {
+        debugLog(config.debug, 1, "loader not found:", loader);
+    }
 
-for (const loader of config.custom ?? []) {
-    await import(`${cwd}/${loader}`).then(async l => {
-        const plugin = await new l.default(cwd).toPlugin();
+
+for (const loader of config.custom ?? []) 
+    try {
+        const l = await import(`${cwd}/${loader}`),
+            plugin = await new l.default(cwd).toPlugin();
+        
         Bun.plugin(plugin);
         debugLog(config.debug, 2, "[CUSTOM]", plugin.name, "has been registered.");
-    }).catch(() => debugLog(config.debug, 1, "[CUSTOM] loader not found:", loader));
-}
+    } catch (e) {
+        debugLog(config.debug, 1, "[CUSTOM] loader not found:", loader);
+    }
